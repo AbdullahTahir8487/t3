e11bea359bef1b9fb63ff4f06010c7865b063170
AbdullahTahir8487 <99714617+AbdullahTahir8487@users.noreply.github.com>
2022-02-21 09:43:28 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+// hhhhhhhhhhhhhhhh
 public class MainActivity extends AppCompatActivity {
 
     @Override
